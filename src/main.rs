1bb78fe6602b31ab8cd7b200d4b140548969d1b9
Stanislau Yanukou <stanislau.yanukou@enmacc.de>
2021-02-25 15:23:27 +0100

    silent

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::env;
 use std::io::{self, Read, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+
     let mut total_bytes = 0;
 
     loop {
@@ -18,5 +21,7 @@ fn main() {
         io::stdout().write_all(&buffer[..num_read]).unwrap();
     }
 
-    eprintln!("{}", total_bytes);
+    if !silent {
+        eprintln!("{}", total_bytes);
+    }
 }
