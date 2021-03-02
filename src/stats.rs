0b8da53d228e783bf4405451cf7a8da55d49b981
Stanislau Yanukou <stanislau.yanukou@enmacc.de>
2021-03-02 17:43:20 +0100

    terminal styling

--- src/stats.rs
+++ src/stats.rs
@@ -1,12 +1,15 @@
-use std::io;
+use std::io::{self, Result, Stderr, Write};
 use std::time::{Duration, Instant};
 
 use crossbeam::channel;
+use crossterm::{cursor, execute, style::{self, Color, PrintStyledContent}, terminal::{Clear,
+                                                                                      ClearType}};
 
 pub fn stats_loop(silent: bool, stats_rx: channel::Receiver<usize>) -> io::Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
     let mut timer = Timer::new();
+    let mut stderr = io::stderr();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
         timer.update();
@@ -14,8 +17,8 @@ pub fn stats_loop(silent: bool, stats_rx: channel::Receiver<usize>) -> io::Resul
         total_bytes += num_bytes;
         if !silent && timer.ready {
             timer.ready = false;
-            eprint!(
-                "\r{} {} [{:.0}b/s]",
+            output_progress(
+                &mut stderr,
                 total_bytes,
                 start.elapsed().as_secs().as_time(),
                 rate_per_second
@@ -40,6 +43,23 @@ struct Timer {
     period: Duration,
     countdown: Duration,
     ready: bool,
+}
+
+fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
+    let bytes = style::style(format!("{} ", bytes)).with(Color::Red);
+    let elapsed = style::style(elapsed).with(Color::Green);
+    let rate = style::style(format!("[{:.0}]b/s", rate)).with(Color::Blue);
+
+    let _ = execute!(
+    stderr,
+    cursor::MoveToColumn(0),
+    Clear(ClearType::CurrentLine),
+    PrintStyledContent(bytes),
+    PrintStyledContent(elapsed),
+    PrintStyledContent(rate),
+    );
+
+    let _ = stderr.flush();
 }
 
 trait TimeOutput {
